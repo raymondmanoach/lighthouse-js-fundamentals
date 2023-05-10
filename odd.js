c4c4a318b067073b5c37cc64589bafdbaee7f625
raymondmanoach <raymondmanoach@gmail.com>
2023-05-10 13:43:42 -0600

    Practice with odd function

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num) {
+    if (num % 2 !== 0) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("47 is odd: " + isOdd(47));
+console.log("28 is odd: " + isOdd(28));
